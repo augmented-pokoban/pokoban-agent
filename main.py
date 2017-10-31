0aeb6debbe9cdfa561efb0e860e9303fe2615678
Anders Rydbirk <ary@it-minds.dk>
2017-10-31 14:13:19 +0100

    Changed number of workers to thread max

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from env.Env import Env
 from Network import Network
 from Worker import Worker
 
-max_episode_length = 301
+max_episode_length = 300
 max_buffer_length = 20
 gamma = .99  # discount rate for advantage estimation and reward discounting
 height = 20
@@ -28,7 +28,7 @@ with tf.device("/cpu:0"):
     global_episodes = tf.Variable(0, dtype=tf.int32, name='global_episodes', trainable=False)
     trainer = tf.train.RMSPropOptimizer(learning_rate=7e-4, epsilon=0.1, decay=0.99)
     master_network = Network(height, width, depth, s_size, a_size, 'global', None)  # Generate global network
-    num_workers = 1  # multiprocessing.cpu_count()  # Set workers ot number of available CPU threads
+    num_workers = multiprocessing.cpu_count()  # Set workers ot number of available CPU threads
 
     print('Creating', num_workers, 'workers')
     workers = []
