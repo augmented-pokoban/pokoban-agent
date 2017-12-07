d8c7986ab21ce089e961264ba4d2e3bff233f87c
Anders Rydbirk <ary@it-minds.dk>
2017-12-07 15:11:51 +0100

    Removed lines for the greater pleasure of Tro-L-S

--- submit.sh
+++ submit.sh
@@ -22,9 +22,3 @@ module load python3/3.5.1
 source /appl/tensorflow/1.3cpu-python3.5/bin/activate
 
 /appl/glibc/2.17/lib/ld-linux-x86-64.so.2  --library-path /appl/glibc/2.17/lib/:/appl/gcc/4.8.5/lib64/:/usr/lib64/atlas:/lib64:/usr/lib64:$LD_LIBRARY_PATH $(which python) main.py
-
-
-
-
-
-
